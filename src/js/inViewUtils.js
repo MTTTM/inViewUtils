2785144d77e9bf4e9749b90e0b0a93895bdec4a2
陆康永 <x13352840347@gmail.com>
2022-02-24 17:01:00 +0800

    完成是否在dom 可视区的检测

--- src/js/inViewUtils.js
+++ src/js/inViewUtils.js
@@ -34,8 +34,59 @@ export const getDomScrollY = function(dom) {
 export const isInView = function({ dom, rotate, otherHeight = 0 }) {
     let rect = getBoundingClientRect({ dom: dom });
     let viewHeight = getViewPortHeight({});
-    console.log("rect", rect, viewHeight);
     return rect.top >= 0 && rect.bottom <= viewHeight - otherHeight;
 };
 
 //是否在dom的可视区
+/**
+ *
+ * @param {*} param0
+ * @description
+ *  dom 检测的目标dom
+ *  wrapDom 容器dom
+ *  overallVisible 是否完全都在可视区
+ */
+export const isInDomView = function({
+    dom,
+    wrapDom,
+    rotate,
+    overallVisible = true,
+}) {
+    let rect = getBoundingClientRect({ dom: dom });
+    let wrapDomReact = getBoundingClientRect({ dom: wrapDom });
+    if (overallVisible) {
+        //整个dom都在可视区nei
+        return (
+            wrapDomReact.bottom >= rect.bottom &&
+            rect.top >= wrapDomReact.top &&
+            rect.left >= wrapDomReact.left &&
+            wrapDomReact.right >= rect.right
+        );
+    } else {
+        //X轴方向范围内
+        let xInView =
+            rect.left >= wrapDomReact.left && wrapDomReact.right >= rect.right;
+        //Y轴一部分在视图里面
+        let yPartInView =
+            (rect.bottom > wrapDomReact.top && rect.bottom < wrapDomReact.bottom) || //一部分超出顶部
+            (rect.top < wrapDomReact.bottom && rect.top > wrapDomReact.top); //一部分超出底部
+        //Y轴全部在shitu
+        let yInView =
+            wrapDomReact.bottom >= rect.bottom && rect.top >= wrapDomReact.top;
+        //X轴一部分在视图
+        let xPartInView =
+            (rect.right > wrapDomReact.left && rect.right < wrapDomReact.right) || //一部分超出左侧
+            (rect.left < wrapDomReact.right && rect.left > wrapDomReact.left); //一部分超出右侧
+        console.log(
+            "xInView",
+            xInView,
+            "yPartInView",
+            yPartInView,
+            "yInView",
+            yInView,
+            "xPartInView",
+            xPartInView
+        );
+        return (xInView && yPartInView) || (yInView && xPartInView);
+    }
+};

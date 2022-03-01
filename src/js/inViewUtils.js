1516ecbfbf2f26d3345a514d5bf8d543901ca4d3
陆康永 <x13352840347@gmail.com>
2022-03-01 18:42:10 +0800

    获取进入可视区的距离

--- src/js/inViewUtils.js
+++ src/js/inViewUtils.js
@@ -1,40 +1,40 @@
 //dom 相关==========================
-export const getBoundingClientRect = function ({ dom, rotate }) {
+export const getBoundingClientRect = function({ dom, rotate = 0 } = {}) {
     return dom.getBoundingClientRect();
 };
 
-export const getRectWidth = function ({ dom, rotate }) {
+export const getRectWidth = function({ dom, rotate = 0 } = {}) {
     let obj = getBoundingClientRect({ dom, rotate });
     return obj.width;
 };
-export const getRectHeight = function ({ dom, rotate }) {
+export const getRectHeight = function({ dom, rotate = 0 } = {}) {
     let obj = getBoundingClientRect({ dom, rotate });
     return obj.height;
 };
 
 //窗口相关
-export const getViewPortHeight = function ({ rotate }) {
+export const getViewPortHeight = function({ rotate = 0 } = {}) {
     return !rotate ? window.innerHeight : window.innerWidth;
 };
-export const getViewPortWidth = function ({ rotate }) {
+export const getViewPortWidth = function({ rotate = 0 } = {}) {
     return !rotate ? window.innerWidth : window.innerHeight;
 };
 //滚动相关
-export const getBodyScrollY = function () {
+export const getBodyScrollY = function() {
     return document.documentElement.scrollTop || document.body.scrollTop;
 };
-export const getBodyScrollX = function () {
+export const getBodyScrollX = function() {
     return document.documentElement.scrollLeft || document.body.scrollLeft;
 };
-export const getDomScrollX = function (dom) {
+export const getDomScrollX = function(dom) {
     return dom.scrollLeft;
 };
-export const getDomScrollY = function (dom) {
+export const getDomScrollY = function(dom) {
     return dom.scrollTop;
 };
 
 //是否在窗口可视区
-export const isInView = function ({ dom, rotate, otherHeight = 0 }) {
+export const isInView = function({ dom, rotate, otherHeight = 0 }) {
     let rect = getBoundingClientRect({ dom: dom });
     let viewHeight = getViewPortHeight({});
     // console.log("otherHeight", otherHeight)
@@ -51,7 +51,7 @@ export const isInView = function ({ dom, rotate, otherHeight = 0 }) {
  *  overallVisible 是否完全都在可视区
  * otherHeight 底部覆盖元素高度
  */
-export const isInDomView = function ({
+export const isInDomView = function({
     dom,
     wrapDom,
     rotate,
@@ -62,7 +62,13 @@ export const isInDomView = function ({
     let wrapDomReact = getBoundingClientRect({ dom: wrapDom });
 
     let wrapDomReactBottom = wrapDomReact.bottom - otherHeight;
-    console.log("otherHeight", otherHeight, wrapDomReactBottom, 'rect.bottom', rect.bottom)
+    console.log(
+        "otherHeight",
+        otherHeight,
+        wrapDomReactBottom,
+        "rect.bottom",
+        rect.bottom
+    );
     if (overallVisible) {
         //整个dom都在可视区nei
         return (
@@ -106,7 +112,7 @@ export const isInDomView = function ({
  * @param {*} dir  检测滚动方向
  * @returns  dom
  */
-export const getScrollableChildren = function (box, maxLoop = 100, dir = "v") {
+export const getScrollableChildren = function(box, maxLoop = 100, dir = "v") {
     let v = 0;
     let direction = dir == "h" ? "h" : "v";
     var result = null;
@@ -134,5 +140,19 @@ export const getScrollableChildren = function (box, maxLoop = 100, dir = "v") {
         }
         return result;
     }
-    return check(box, maxLoop)
-}
+    return check(box, maxLoop);
+};
+/**
+ *  获取dom到完全可见的距离
+ * @param {*} dom
+ * @param {*} viewPort 可能是dom，可能是window
+ * @param {*} otherHeight
+ */
+export const getDomToVisbleDis = function(dom, viewPort, otherHeight = 0) {
+    let rect = getBoundingClientRect({ dom });
+    if (viewPort instanceof Element) {} else {
+        //dom在容器dom可视区的距离
+        return rect.bottom - getViewPortHeight() + otherHeight;
+    }
+    return null;
+};
